d9fc38769b38d4965e8158748cf8cf9dab8cdfe6
gasiorMateusz <mateuszgasior314@gmail.com>
2023-01-10 16:28:50 +0100

    implement basic logic handling arguments

--- src/main/java/com/codecool/converter/ConverterApplication.java
+++ src/main/java/com/codecool/converter/ConverterApplication.java
@@ -1,14 +1,45 @@
 package com.codecool.converter;
 
+import java.io.File;
 import java.util.Arrays;
 
 public class ConverterApplication {
 
     public static void main(String[] args) {
-        if(!Arrays.stream(args).findAny().isPresent()){
+        if (!Arrays.stream(args).findAny().isPresent()) {
             System.out.println("No input file defined");
-        }else {
-            System.out.println(args);
+        } else if (args.length == 1 && utils.isValidPath(args[0])) {
+            ifArgValidDisplayCSV(args);
+        } else if (args.length == 2) {
+            ifArgsValidExecuteConvertion(args);
+        } else{
+            System.out.println("invalid parameters");
+        }
+
+    }
+
+    private static void ifArgValidDisplayCSV(String[] args) {
+        //path -> display file data in table fixme
+        File file = new File(args[0]);
+    }
+
+    private static void ifArgsValidExecuteConvertion(String[] args) {
+        String format = null;
+        if(utils.isValidPath(args[0])){
+            File file = new File(args[0]);
+            format = args[1];
+        }else if(utils.isValidPath(args[1])){
+            File file = new File(args[1]);
+            format = args[0];
+        }
+        if (format != null && !format.isEmpty()) {
+            if (format.equalsIgnoreCase("xml")) {
+                //it is xml fixme
+            } else if (format.equalsIgnoreCase("json")) {
+                //it is json fixme
+            } else {
+                System.out.println("unsupported extension type");
+            }
         }
     }
 
