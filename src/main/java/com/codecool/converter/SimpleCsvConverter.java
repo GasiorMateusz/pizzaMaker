629f695fa127446b41ed0ac1846a79d21d6dde4b
gasiorMateusz <mateuszgasior314@gmail.com>
2023-01-11 13:04:51 +0100

    implement fileReader

--- src/main/java/com/codecool/converter/SimpleCsvConverter.java
+++ src/main/java/com/codecool/converter/SimpleCsvConverter.java
@@ -5,10 +5,12 @@ import java.io.File;
 public class SimpleCsvConverter {
 
     ConverterTypeManager converterType;
+    FileReader fileReader = new FileReader();
 
-    public void converter(File file) {
+    public void converter(File file, FileFormat... outputFormat) {
+
         System.out.println("I convert CSV to output format");
-        converterType.executeConversion(file);
+        converterType.executeConversion(fileReader.readData(file));
     }
 
     public void setType(ConverterTypeManager converterType) {
