1345158441a4841bcbf37bc1acc773112cc54355
gasiorMateusz <mateuszgasior314@gmail.com>
2023-01-11 13:04:29 +0100

    add FileReader

---
+++ src/main/java/com/codecool/converter/FileReader.java
@@ -0,0 +1,18 @@
+package com.codecool.converter;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FileReader {
+
+    public Scanner readData(File file) {
+        System.out.println("read a file");
+//        try {
+//            Scanner scanner = new Scanner(new File("book.csv"));
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+        return null;
+    }
+
+}
